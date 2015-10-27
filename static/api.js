eae4d24788f5e497e01f41b4649ccdde49a55deb
Roderic Day <roderic.day@gmail.com>
2015-10-27 09:45:10 -0400

    Delete from website

--- static/api.js
+++ static/api.js
@@ -1,17 +1,37 @@
 function submitForm() {
-    $.post('/autos', $("#mainForm").serialize());
-    updateCollection();
-    this.reset();
+    var form = this;
+    $.ajax({
+        url: '/autos',
+        type: 'POST',
+        data: $("#mainForm").serialize(),
+        success: function() { form.reset(); updateCollection(); }
+    });
     return false
 }
 
+function promptDelete(entryId) {
+    if (confirm("Are you sure you want to delete this entry?")) {
+        $.ajax({
+            url: '/autos/'+entryId,
+            type: 'DELETE',
+            success: updateCollection
+        });
+    }
+}
+
 function updateCollection() {
+    collectionView.innerHTML = '';
     $.getJSON('/autos', null, function(response) {
+        var i = 0; // add internal keys to avoid this mess
         response.data.forEach(function(entry){
             var copy = document.querySelector("#entryTemplate").cloneNode(true);
+            copy.id = i++;
             for (key in entry) {
-                document.querySelector('.'+key).innerHTML = entry[key];
+                copy.querySelector('.'+key).innerHTML = entry[key];
             }
+            copy.querySelector(".buttonDelete").onclick = function() {
+                promptDelete(copy.id)
+            };
             collectionView.appendChild(copy);
         });
     });
